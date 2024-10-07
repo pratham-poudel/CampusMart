1134cd838772fde2b64f9daa996e425f800066ee
Pratham Poudel <prathampoudel2@gmail.com>
2024-10-07 13:23:09 +0530

    process.env.PORT updated

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.set('view engine', 'ejs');
 
 
 // Start the server
-app.listen(3000, () => {
+app.listen(process.env.PORT||3000, () => {
     console.log('Server is running on port 3000');
 });
